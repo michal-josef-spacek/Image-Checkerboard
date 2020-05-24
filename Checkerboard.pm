9b5725c311ddba3919224a5cb20947484b67184b
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-05-24 18:31:46 +0200

    Improve LICENSE AND COPYRIGHT section in doc

--- Checkerboard.pm
+++ Checkerboard.pm
@@ -287,8 +287,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2012-2020 Michal Josef Špaček
- BSD 2-Clause License
+© 2012-2020 Michal Josef Špaček
+
+BSD 2-Clause License
 
 =head1 VERSION
 
